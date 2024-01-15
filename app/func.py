6ffa63c4637b1cc3ef46d063c2ec1c948b4f7778
Muhammad Rafi Ali <126326371+therafiali@users.noreply.github.com>
2024-01-16 01:23:52 +0500

    Update func.py

--- app/func.py
+++ app/func.py
@@ -1,13 +1,1 @@
-import requests
-import json
-
-
-BASE_URL = "http://127.0.0.1:8000/"
-
-# Function to get all todos from the API
-def get_all_todos():
-    """Get List of all Todos"""
-    response = requests.get(BASE_URL)  # Send GET request to API
-    res_json = response.json()  # Convert response to JSON
-    res_str = json.dumps(res_json)  # Convert JSON to string
-    return res_str  # Return string
+#no file
